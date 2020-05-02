c958442b2a22147d5504f1c72e9be698e9cdd84a
zhangjing <jing_zhang@smartoceantech.com>
2020-05-02 23:21:32 +0800

    [Modify]菜单模块初步完成，还需进一步调试

--- 1_src/3-console_menu/menu.c
+++ 1_src/3-console_menu/menu.c
@@ -54,6 +54,55 @@ int menu_display(MENU_T *menus)
 
     return 0;
 }
+static MENU_T menu2_3 = {
+    .id         = 3,
+    .text_info  = "menu2_3",
+};
+static MENU_T menu2_2 = {
+    .id         = 2,
+    .text_info  = "menu2_2",
+};
+static MENU_T menu2_1 = {
+    .id         = 1,
+    .text_info  = "menu2_1",
+};
+
+static MENU_T menu3_3 = {
+    .id         = 3,
+    .text_info  = "menu3_3",
+};
+static MENU_T menu3_2 = {
+    .id         = 2,
+    .text_info  = "menu3_2",
+};
+static MENU_T menu3_1 = {
+    .id         = 1,
+    .text_info  = "menu3_1",
+};
+static MENU_T menu1_3 = {
+    .id         = 3,
+    .text_info  = "menu1_3",
+};
+static MENU_T menu1_2 = {
+    .id         = 2,
+    .text_info  = "menu1_2",
+};
+static MENU_T menu1_1 = {
+    .id         = 1,
+    .text_info  = "menu1_1",
+};
+static MENU_T menu3 = {
+    .id         = 3,
+    .text_info  = "[第一层][第三项]",
+};
+static MENU_T menu2 = {
+    .id         = 2,
+    .text_info  = "[第一层][第二项]",
+};
+static MENU_T menu1 = {
+    .id         = 1,
+    .text_info  = "[第一层][第一项]",
+};
 
 int func_menu1()
 {
@@ -78,46 +127,33 @@ int func_menu2()
 
     return 0;
 }
-
-static MENU_T menu2_1 = {
-    .id         = 1,
-    .text_info  = "menu2_1",
-    .next       = NULL,
-    .sub_menus  = NULL,
-    .func       = NULL
-};
-
-static MENU_T menu1_1 = {
-    .id         = 1,
-    .text_info  = "menu1_1",
-    .next       = NULL,
-    .sub_menus  = NULL,
-    .func       = NULL
-};
-
-static MENU_T menu2 = {
-    .id         = 2,
-    .text_info  = "[第一层][第二项]",
-    .next       = NULL,
-    .sub_menus  = &menu2_1,
-    .func       = func_menu2
-};
-
-/*！
- * \brief 第一层菜单的第一条菜单项
- */
-static MENU_T menu1 = {
-    .id         = 1,
-    .text_info  = "[第一层][第一项]",
-    .next       = &menu2,
-    .sub_menus  = &menu1_1,
-    .func       = func_menu1
-};
-
-static MENU_T *main_menu;
-int menus_init()
+int func_menu3()
 {
-    main_menu = menu1;
+    char *str;
+
+    pr_info_pure("[主菜单第三项]请输入\n");
+    memset(ibuf, 0, sizeof(ibuf));
+    scanf("%s", ibuf);
+    pr_info_pure("你输入的是: %s\n", ibuf);
+
+    return 0;
+}
+
+int func_menu1_1(){printf("%s\n", __func__);}
+int func_menu1_2(){printf("%s\n", __func__);}
+int func_menu1_3(){printf("%s\n", __func__);}
+int func_menu2_1(){printf("%s\n", __func__);}
+int func_menu2_2(){printf("%s\n", __func__);}
+int func_menu2_3(){printf("%s\n", __func__);}
+int func_menu3_1(){printf("%s\n", __func__);}
+int func_menu3_2(){printf("%s\n", __func__);}
+int func_menu3_3(){printf("%s\n", __func__);}
+
+MENU_T *menu_init()
+{
+    MENU_T *main_menu = NULL;
+
+    main_menu = &menu1;
     /** 第一级菜单指针建立 */
     menu1.next = &menu2;
     menu2.next = &menu3;
@@ -142,16 +178,65 @@ int menus_init()
     menu3_2.next = &menu3_3;
     menu3_3.next = NULL;
 
-    return 0;
+    /** 第三级菜单指针建立 */
+    menu1_1.func = func_menu1_1;
+    menu1_1.sub_menus = NULL;
+    menu1_2.func = func_menu1_2;
+    menu1_2.sub_menus = NULL;
+    menu1_3.func = func_menu1_3;
+    menu1_3.sub_menus = NULL;
+    menu2_1.func = func_menu2_1;
+    menu2_1.sub_menus = NULL;
+    menu2_2.func = func_menu2_2;
+    menu2_2.sub_menus = NULL;
+    menu2_3.func = func_menu2_3;
+    menu2_3.sub_menus = NULL;
+    menu3_1.func = func_menu3_1;
+    menu3_1.sub_menus = NULL;
+    menu3_2.func = func_menu3_2;
+    menu3_2.sub_menus = NULL;
+    menu3_3.func = func_menu3_3;
+    menu3_3.sub_menus = NULL;
+
+    return main_menu;
+}
+
+MENU_T *menu_enter(MENU_T *menu, int id)
+{
+    MENU_T *m;
+
+    for (m = menu; m; m = m->next) {
+        if (m->id == id)
+            break;
+    }
+
+    if (!m)
+        return NULL;
+
+    if (m->func)
+        m->func(m);
+
+    return m->sub_menus;
 }
 
 #define CONSOLE_MENU_UNITEST
 #ifdef  CONSOLE_MENU_UNITEST
 int main()
 {
+    int id;
+    MENU_T *menu;
 
-    menu_display(&menu1);
+    menu = menu_init();
+
+    /*!
+     * 循环展示一级菜单，等待选择菜单项，选定后进入子菜单或者执行菜单内容，然后继续循环
+     */
+    while (1) {
+        menu_display(menu);
+        printf("请输入序号\n");
+        scanf("%d", &id);
+        menu = menu_enter(menu, id);
+    }
 }
-
 
 #endif /* CONSOLE_MENU_UNITEST */
