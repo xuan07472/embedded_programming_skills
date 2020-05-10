ca5c2e54c313af2877389755359f20da1e75fe4e
zhangjing <jing_zhang@smartoceantech.com>
2020-05-10 21:30:08 +0800

    [Modify]

--- 1_src/6-module-route/main.c
+++ 1_src/6-module-route/main.c
@@ -1,10 +1,23 @@
-#define MODULE_COUNT 3
-static struct xxx module[MODULE_COUNT];
-enum {
-    NONE,
-    INITIALLISED,
-    STARTED,
-    RUNNING
+// 适用于无操作系统，无线程同步，但有多模块相互之间两两通信的场景
+
+#include <stdio.h>
+
+#define MODULES_COUNT 3
+#define ECOMM       (-1)
+#define pr_err printf
+
+typedef int (*FUNC)(void);
+typedef struct {
+    char *name;
+    FUNC init;
+    FUNC run;
+    FUNC exit;
+} MODULE_T;
+
+MODULE_T modules[MODULES_COUNT] = {
+    {"模块1", module1_init, module1_run, module1_exit},
+    {"模块1", module2_init, module2_run, module2_exit},
+    {"模块1", module3_init, module3_run, module3_exit}
 };
 
 int main()
@@ -13,28 +26,21 @@ int main()
     int ret;
 
     /** 初始化 */
-    for (i = 0; i < MODULE_COUNT; i++) {
-        ret = -1; //TODO: 增加错误号
-        if (module[i].init)
-            ret = module[i].init();
-        if (ret >= 0)
-            module[i].running_state = INITIALLISED;
+    for (i = 0; i < MODULES_COUNT; i++) {
+        ret = ECOMM;
+        if (modules[i].init)
+            ret = modules[i].init();
+        if (ret) {
+            pr_err("Module[%d]%s init error!\n", i, modules[i].name);
+            return ECOMM;
+        }
     }
 
-    /** 开始准备 */
-    for (i = 0; i < MODULE_COUNT; i++) {
-        ret = -1; //TODO: 增加错误号
-        if (module[i].start && (module[i].running_state == INITIALLISED))
-            ret = module[i].start();
-        if (ret >= 0)
-            module[i].running_state = STARTED;
-    }
-
-    /** 运行中 */
+    /** 运行 */
     while (1) {
-        for (i = 0; i < MODULE_COUNT; i++) {
-            if (module[i].run && (module[i].running_state == STARTED))
-                module[i].run();
+        for (i = 0; i < MODULES_COUNT; i++) {
+            if (modules[i].run)
+                modules[i].run();
         }
     }
 }
