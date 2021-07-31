8154e43873a6f38eb22b8c531d158d89f150bf65
zhangjing <zhangjing@dwin.com.cn>
2021-07-31 18:18:58 +0800

    [Modify] 增加日志控制

--- 1_src/08-file_string_rand/main.c
+++ 1_src/08-file_string_rand/main.c
@@ -12,8 +12,9 @@
  * @date	2021年7月31日
  * @文件编码	UTF-8 Linux换行
  * @编译方式	1、Linux下或者Windows的git MINGW64命令行界面make clean; make
- * 		2、Windows下Qt或者Visual Studio自行新建工程并编译
- * @源码地址
+ *		2、Windows下Qt或者Visual Studio自行新建工程并编译
+ * @源码地址	https://gitee.com/langcai1943/embedded_programming_skills/tree/develop/1_src/08-file_string_rand
+ * @视频教程	https://space.bilibili.com/106424039
  *
  * ----------------------------------------------------------------------------*
  * 修改记录:                                                                   *
@@ -24,16 +25,59 @@
  *
  ******************************************************************************/
 
-/*=================================== 头文件 =================================*/
+/*================================== 头文件 ==================================*/
 #include <stdio.h>
 
-/*=================================== 宏定义 =================================*/
-#define filename "弹幕-正式版.txt"
+/*==================== 类型定义（struct、 enum 和 typedef） ==================*/
 
-/*===================================  =================================*/
+/*================================== 宏定义 ==================================*/
+#define DEFAULT_FILE_NAME "弹幕-正式版.txt"
 
-/*================================== 接口函数 ================================*/
-int main()
+/**
+ * @brief	重定义输出级别控制
+ * @details	- ...和__VA_ARGS__一起实现宏定义函数的调用
+ * 		- #和##实现宏定义函数的变量名转成字符串，和变量名拼接并转字符串；
+ *		- printf第一个参数可以用多个"" ""拼起来作为一个参数，高级语言也有类似用法；
+ * @param[in]	level	输出级别
+ * @param[in]	pure	输出时是否自动加上额外前缀信息
+ */
+#define DETAIL	4
+#define DEBUG	3
+#define INFO	2
+#define RELEASE	1
+#define NOPRINT	0
+#define LOG  1
+#define PURE 0
+#define CUR_PRINT_LEVEL DEBUG /** 定义输出级别 */
+#define print(level, pure, ...)	do { \
+				if ((level) <= CUR_PRINT_LEVEL && (level) != NOPRINT) { \
+					if (pure == LOG) \
+						printf("["#level"] "); \
+					printf(__VA_ARGS__); \
+				} \
+			} while (0);
+
+/*================================= 全局变量 =================================*/
+static int readfile_and_print(const char *file);
+static const char *m_filename;
+
+/*================================= 接口函数 =================================*/
+int main(int argc, void *argv[])
 {
-	printf("filename: %s\n", filename);
+	m_filename = DEFAULT_FILE_NAME;
+	
+	if (argc > 1) { /** 参数1默认是应用程序路径，参数2开始才是用户数据 */
+		print(DEBUG, PURE, "str: %s\n", argv[1]);
+	}
+
+	readfile_and_print(m_filename);
 }
+
+/*================================= 接口函数 =================================*/
+static int readfile_and_print(const char *filename)
+{
+
+}
+
+/*================================= 文件结尾 =================================*/
+
